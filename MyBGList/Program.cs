832a18df9e6d88454a5421cedd0e2d6321d9ca01
Jean Jacques Uzabumuhire <uzabumuhire@gmail.com>
2022-06-09 06:08:52 +0200

    Configure Cross-Origin Resource Sharing (CORS)

--- MyBGList/Program.cs
+++ MyBGList/Program.cs
@@ -1,9 +1,31 @@
+using Microsoft.AspNetCore.Cors;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 // Controller service
 builder.Services.AddControllers();
+
+// Implementing CORS
+builder.Services.AddCors(options => {
+
+    // A default policy which accepts every HTTP header and
+    // method only from a restricted set of known origins
+    options.AddDefaultPolicy(cfg => {
+        cfg.WithOrigins(builder.Configuration["AllowedOrigins"]);
+        cfg.AllowAnyHeader();
+        cfg.AllowAnyMethod();
+    });
+
+    // Named policy that accepts everything from everyone
+    options.AddPolicy(name: "AnyOrigin",
+        cfg => {
+            cfg.AllowAnyOrigin();
+            cfg.AllowAnyHeader();
+            cfg.AllowAnyMethod();
+        });
+});
 
 // Swagger services
 // Learn more about configuring Swagger/OpenAPI
@@ -33,14 +55,19 @@ else
 // HTTP to HTTPS redirection middleware
 app.UseHttpsRedirection();
 
+// Applying CORS
+app.UseCors();
+
 // ASP.NET Core authorization middleware
 app.UseAuthorization();
 
 // Handles exceptions for end users
-app.MapGet("/error", () => Results.Problem());
+app.MapGet("/error", [EnableCors("AnyOrigin")] () => 
+    Results.Problem());
 
-// For testing exception handling
-app.MapGet("/error/test", () => { throw new Exception("test"); });
+// For testing exceptions handling
+app.MapGet("/error/test", [EnableCors("AnyOrigin")] () => 
+    { throw new Exception("test"); });
 
 // Controller middleware
 app.MapControllers();
