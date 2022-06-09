f23ea6c580690b194fe935955a48c3a63f1e9eec
Jean Jacques Uzabumuhire <uzabumuhire@gmail.com>
2022-06-09 06:41:14 +0200

    Cache a list of Board Games for a reasonable amount of time

--- MyBGList/Controllers/BoardGamesController.cs
+++ MyBGList/Controllers/BoardGamesController.cs
@@ -14,6 +14,7 @@ namespace MyBGList.Controllers
         }
 
         [HttpGet(Name = "GetBoardGames")]
+        [ResponseCache(Location = ResponseCacheLocation.Any, Duration = 60)]
         public IEnumerable<BoardGame> Get()
         {
             return new[] {
