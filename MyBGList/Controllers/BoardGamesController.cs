2b1de4dd5352ff578cebdd2b6502a5961f6a98e2
Jean Jacques Uzabumuhire <uzabumuhire@gmail.com>
2022-06-11 00:44:50 +0200

    Change the existing anonymous type with the RestDTO  which contains the data and the descriptive links.

--- MyBGList/Controllers/BoardGamesController.cs
+++ MyBGList/Controllers/BoardGamesController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using MyBGList.DTO;
+
 namespace MyBGList.Controllers
 {
     [Route("[controller]")]
@@ -15,32 +17,44 @@ namespace MyBGList.Controllers
 
         [HttpGet(Name = "GetBoardGames")]
         [ResponseCache(Location = ResponseCacheLocation.Any, Duration = 60)]
-        public IEnumerable<BoardGame> Get()
+        //[ResponseCache(NoStore = true)]
+        public RestDTO<BoardGame[]> Get()
         {
-            return new[] {
-                new BoardGame() {
+            return new RestDTO<BoardGame[]>() 
+            {
+                Data = new BoardGame[] {
+                    new BoardGame() {
                     Id = 1,
                     Name = "Axis & Allies",
                     Publisher = "Milton Bradley",
                     Year = 1981,
                     MinPlayers = Random.Shared.Next(4),
                     MaxPlayers = Random.Shared.Next(5, 9)
+                    },
+                    new BoardGame() {
+                        Id = 2,
+                        Name = "Citadel",
+                        Publisher = "Hans im Glück",
+                        Year = 2000,
+                        MinPlayers = Random.Shared.Next(4),
+                        MaxPlayers = Random.Shared.Next(5, 9)
+                    },
+                    new BoardGame() {
+                        Id = 3,
+                        Name = "Terraforming Mars",
+                        Publisher = "FryxGames",
+                        Year = 2016,
+                        MinPlayers = Random.Shared.Next(4),
+                        MaxPlayers = Random.Shared.Next(5, 9)
+                    }
                 },
-                new BoardGame() {
-                    Id = 2,
-                    Name = "Citadel",
-                    Publisher = "Hans im Glück",
-                    Year = 2000,
-                    MinPlayers = Random.Shared.Next(4),
-                    MaxPlayers = Random.Shared.Next(5, 9)
-                },
-                new BoardGame() {
-                    Id = 3,
-                    Name = "Terraforming Mars",
-                    Publisher = "FryxGames",
-                    Year = 2016,
-                    MinPlayers = Random.Shared.Next(4),
-                    MaxPlayers = Random.Shared.Next(5, 9)
+
+                Links = new List<LinkDTO>
+                {
+                    new LinkDTO(
+                        Url.Action(null, "BoardGames", null, Request.Scheme)!,
+                        "self",
+                        "GET")
                 }
             };
         }
